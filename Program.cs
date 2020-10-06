de12197fa160abb10971c946400c6876f1d5846b
ÄGARE <GARE@DESKTOP-1SVJT58>
2020-10-06 11:51:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Consoletest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Skriv in ditt nummer!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number < 50)
+            {
+                Console.WriteLine("Ditt nummer är mindre än 50!");
+            }
+
+            if (number > 50)
+            {
+                Console.WriteLine("Ditt nummer är högre än 50!");
+            }
+            Console.Write("Ditt nummer äer " + number);
+
+
+
+
+            {
+
+            }
+
+
+
+
+        }
+    }
+}
